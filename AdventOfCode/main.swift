7e1ddc3927ee03707715bb019d0337dbc21d35d6
Daniel <daniel.johns@robotsandpencils.com>
2022-11-09 10:55:20 -0800

    Create Day prototype

--- AdventOfCode/main.swift
+++ AdventOfCode/main.swift
@@ -1,11 +1,39 @@
 //
 //  main.swift
-//  AdventOfCode
+//  Advent of Code
 //
-//  Created by DanielJohns on 2022-11-09.
+//  Created by DanielJohns on 2022-11-08.
 //
 
 import Foundation
+import AppKit
 
-print("Hello, World!")
+func run(_ day: Int) {
+    let url = FileManager.default.homeDirectoryForCurrentUser
+        .appendingPathComponent("Documents", isDirectory: true)
+        .appendingPathComponent("AdventOfCode", isDirectory: true)
+        .appendingPathComponent("Day\(day)", isDirectory: true)
+        .appendingPathComponent("input", isDirectory: false)
+    
+    guard let input = try? String(contentsOf: url) else {
+        debugPrint("Unable to find input for day \(day)")
+        return
+    }
+    
+    guard let dayClass = Bundle.main.classNamed("AdventOfCode.Day\(day)") as? Day.Type else {
+        debugPrint("Unable to create Day class for \(day)")
+        return
+    }
+    
+    let day = dayClass.init()
+    let result = day.run(input: input)
+    print(result)
+    NSPasteboard.general.setString(result, forType: .string)
+}
+
+if let day = Int(CommandLine.arguments.dropFirst().first ?? "") {
+    run(day)
+} else {
+    debugPrint("Missing day argument")
+}
 
