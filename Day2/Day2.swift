9a30a36687144ed974e3ace1d10c761f19133480
Daniel <civatrix@gmail.com>
2022-12-01 23:02:31 -0800

    Update day 2 to simpler solution

--- Day2/Day2.swift
+++ Day2/Day2.swift
@@ -8,54 +8,22 @@
 import Foundation
 
 final class Day2: Day {
-    enum Play: Character, Comparable {
-        case rock = "A"
-        case paper = "B"
-        case scissors = "C"
-        
-        static let order: [Play] = [.rock, .paper, .scissors, .rock]
-        
-        var throwScore: Int {
-            switch self {
-            case .rock: return 1
-            case .paper: return 2
-            case .scissors: return 3
-            }
-        }
-        
-        func playToAchieve(result: Character) -> Play {
-            switch result {
-            case "X": return Play.order[Play.order.lastIndex(of: self)! - 1]
-            case "Y": return self
-            case "Z": return Play.order[Play.order.firstIndex(of: self)! + 1]
-            default: exit(2)
-            }
-        }
-        
-        static func < (lhs: Play, rhs: Play) -> Bool {
-            return (lhs == .rock && rhs == .paper) ||
-            (lhs == .paper && rhs == .scissors) ||
-            (lhs == .scissors && rhs == .rock)
-        }
-    }
-    
     func run(input: String) -> String {
-        let lines = input.lines
-                
-        var score = 0
-        for line in lines {
-            let opponent = Play(rawValue: line.first!)!
-            let you = opponent.playToAchieve(result: line.last!)
-            
-            score += you.throwScore
-            
-            if opponent == you {
-                score += 3
-            } else if opponent < you {
-                score += 6
-            }
-        }
+        let map = [
+            "A X": 3 + 0,
+            "A Y": 1 + 3,
+            "A Z": 2 + 6,
+            "B X": 1 + 0,
+            "B Y": 2 + 3,
+            "B Z": 3 + 6,
+            "C X": 2 + 0,
+            "C Y": 3 + 3,
+            "C Z": 1 + 6,
+        ]
         
-        return score.description
+        return input.lines
+            .map { map[$0]! }
+            .sum
+            .description
     }
 }
