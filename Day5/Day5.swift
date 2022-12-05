cfd162723772d9ced65b1fc6352143c0d2303c31
Daniel <civatrix@gmail.com>
2022-12-04 21:34:43 -0800

    Day 5 Part 1

--- Day5/Day5.swift
+++ Day5/Day5.swift
@@ -9,6 +9,27 @@ import Foundation
 
 final class Day5: Day {
     func run(input: String) -> String {
-        return ""
+        let crates = input.allLines.split(separator: "")[0]
+        let stackCount = crates.last!.allDigits.last!
+        var stacks = [[Character]](repeating: [], count: stackCount)
+        for line in crates {
+            for (index, char) in line.enumerated() {
+                guard char.isLetter else { continue }
+                stacks[(index - 1) / 4].insert(char, at: 0)
+            }
+        }
+        
+        // Remove the stack identifiers
+        let instructions = input.allDigits.dropFirst(stackCount)
+        for instruction in instructions.chunks(ofCount: 3) {
+            let count = instruction[instruction.startIndex]
+            let start = instruction[instruction.startIndex.advanced(by: 1)] - 1
+            let end = instruction[instruction.startIndex.advanced(by: 2)] - 1
+            for _ in (0 ..< count) {
+                stacks[end].append(stacks[start].popLast()!)
+            }
+        }
+        
+        return String(stacks.map { $0.last! })
     }
 }
--- Day5/Day5Tests.swift
+++ Day5/Day5Tests.swift
@@ -11,7 +11,18 @@ final class Day5Tests: XCTestCase {
     let day = Day5()
     
     func testDay() throws {
-        let input = ""
-        XCTAssertEqual(day.run(input: input), "")
+        let input =
+"""
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"""
+        XCTAssertEqual(day.run(input: input), "CMZ")
     }
 }
