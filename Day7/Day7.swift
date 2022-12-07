badd44237078ffd4d2495943fe3d7cb639bec15d
Daniel <civatrix@gmail.com>
2022-12-07 10:34:27 -0800

    Day 7 Part 1

--- Day7/Day7.swift
+++ Day7/Day7.swift
@@ -7,8 +7,44 @@
 
 import Foundation
 
+class Directory {
+    var children: [Substring: Directory]
+    var parent: Directory!
+    var fileSizes: [Int] = []
+    
+    init(children: [Substring : Directory] = [:], parent: Directory! = nil) {
+        self.children = children
+        self.parent = parent
+    }
+    
+    var totalSizes: [Int] {
+        let childSizes = children.values.flatMap { $0.totalSizes }
+        return childSizes + [fileSizes.sum + childSizes.sum]
+    }
+}
+
 final class Day7: Day {
     func run(input: String) -> String {
-        return ""
+        let root = Directory()
+        var current = root
+        
+        for line in input.lines {
+            if line.hasPrefix("$") {
+                guard line.hasPrefix("$ cd") else { continue }
+                
+                let target = line.split(separator: " ").last!
+                switch target {
+                case "..": current = current.parent
+                case "/": current = root
+                default: current = current.children[target]!
+                }
+            } else if line.hasPrefix("dir") {
+                current.children[line.split(separator: " ").last!] = Directory(parent: current)
+            } else {
+                current.fileSizes.append(Int(line.split(separator: " ").first!)!)
+            }
+        }
+        
+        return root.totalSizes.filter { $0 <= 100000 }.sum.description
     }
 }
--- Day7/Day7Tests.swift
+++ Day7/Day7Tests.swift
@@ -13,7 +13,30 @@ final class Day7Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "95437")
     }
 }
