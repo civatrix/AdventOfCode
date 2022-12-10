b78cf08429c57edbe2378dd16480da3100e6b6ac
Daniel <civatrix@gmail.com>
2022-12-09 21:19:03 -0800

    Day 10 Part 1

--- Day10/Day10.swift
+++ Day10/Day10.swift
@@ -9,6 +9,27 @@ import Foundation
 
 final class Day10: Day {
     func run(input: String) -> String {
-        return ""
+        let targetCycles = Set([20, 60, 100, 140, 180, 220])
+        
+        var register = 1
+        var cycle = 1
+        var signal = 0
+        for line in input.lines {
+            if targetCycles.contains(cycle) {
+                signal += cycle * register
+            }
+            if line.hasPrefix("noop") {
+                cycle += 1
+            } else {
+                if targetCycles.contains(cycle + 1) {
+                    signal += (cycle + 1) * register
+                }
+                let value = Int(line.split(separator: " ")[1])!
+                register += value
+                cycle += 2
+            }
+        }
+        
+        return signal.description
     }
 }
