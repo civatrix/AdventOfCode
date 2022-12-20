49acd0f42f8a6625973651d6f58eb2e22dd2b3a0
Daniel <civatrix@gmail.com>
2022-12-19 16:57:18 -0800

    Day 19 Part 1

--- Day19/Day19.swift
+++ Day19/Day19.swift
@@ -8,7 +8,82 @@
 import Foundation
 
 final class Day19: Day {
+    let ore = 0
+    let clay = 1
+    let obsidian = 2
+    let geode = 3
+        
+    struct CacheKey: Hashable {
+        let resources: [Int]
+        let robots: [Int]
+        let timeLeft: Int
+    }
+    
     func run(input: String) -> String {
-        return ""
+        return input.lines
+            .map {
+                let numbers = $0.allDigits
+                
+                return [
+                    [numbers[1], 0, 0, 0],
+                    [numbers[2], 0, 0, 0],
+                    [numbers[3], numbers[4], 0, 0],
+                    [numbers[5], 0, numbers[6], 0],
+                ]
+            }
+            .map(run(blueprint:))
+            .enumerated()
+            .map { ($0.offset + 1) * $0.element }
+            .sum
+            .description
+    }
+    
+    func run(blueprint: [[Int]]) -> Int {
+        var bestCount = 0
+        var queue = [CacheKey]()
+        queue.append(CacheKey(resources: [0,0,0,0], robots: [1,0,0,0], timeLeft: 24))
+        
+        let maxCosts = (ore ... geode).map { key in blueprint.map { $0[key] }.max()! }
+        
+        func addToQueue(_ key: CacheKey) {
+            guard key.timeLeft >= 0 else { return }
+            let geodes = key.resources[geode]
+            if geodes > bestCount {
+                bestCount = geodes
+                queue = queue.filter { key in
+                    geodes + key.robots[geode] * key.timeLeft + (1 ... key.timeLeft).sum >= bestCount
+                }
+            }
+            
+            if key.timeLeft > 0 && geodes + key.robots[geode] * key.timeLeft + (1 ... key.timeLeft).sum >= bestCount {
+                queue.append(key)
+            }
+        }
+        
+        while let key = queue.popLast() {
+            let doNothingResources = zip(key.resources, key.robots).map { $0.0 + (key.timeLeft * $0.1) }
+            addToQueue(.init(resources: doNothingResources, robots: key.robots, timeLeft: 0))
+            
+            for robot in (ore ... geode) {
+                guard robot == geode || key.robots[robot] < maxCosts[robot] else { continue }
+                guard zip(key.robots, blueprint[robot]).allSatisfy({ $1 == 0 || $0 != 0}) else { continue }
+                
+                var newResources = key.resources
+                var timePassed = 0
+                while !zip(newResources, blueprint[robot]).allSatisfy({ $0.0 >= $0.1 }) {
+                    newResources = zip(newResources, key.robots).map { $0.0 + $0.1 }
+                    timePassed += 1
+                }
+                
+                newResources = zip(newResources, key.robots).map { $0.0 + $0.1 }
+                timePassed += 1
+                
+                var newRobots = key.robots
+                newRobots[robot] += 1
+                addToQueue(.init(resources: zip(newResources, blueprint[robot]).map { $0.0 - $0.1 }, robots: newRobots, timeLeft: key.timeLeft - timePassed))
+            }
+        }
+        
+        return bestCount
     }
 }
--- Day19/Day19Tests.swift
+++ Day19/Day19Tests.swift
@@ -13,7 +13,9 @@ final class Day19Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "33")
     }
 }
