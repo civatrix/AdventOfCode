b201948be5360c92a283954eae51f599e339e3e3
Daniel <civatrix@gmail.com>
2022-12-24 09:19:05 -0800

    Day 24 Part 1

--- Day24/Day24.swift
+++ Day24/Day24.swift
@@ -9,6 +9,78 @@ import Foundation
 
 final class Day24: Day {
     func run(input: String) -> String {
-        return ""
+        let lines = input.lines
+        let width = lines[0].count - 2
+        let height = lines.count - 2
+        let start = Point(x: lines[0].distance(from: lines[0].startIndex, to: lines[0].firstIndex(of: ".")!), y: 0)
+        let end = Point(x: lines.last!.distance(from: lines.last!.startIndex, to: lines.last!.firstIndex(of: ".")!), y: lines.count - 1)
+        
+        var windStarts = [Point: Set<Point>]()
+        var walls = Set<Point>([start + .up, end + .down])
+        for (row, line) in input.lines.enumerated() {
+            for (column, char) in line.enumerated() {
+                let point = Point(x: column, y: row)
+                switch char {
+                case ">": windStarts[.right, default: []].insert(point)
+                case "<": windStarts[.left, default: []].insert(point)
+                case "^": windStarts[.up, default: []].insert(point)
+                case "v": windStarts[.down, default: []].insert(point)
+                case "#": walls.insert(point)
+                default: break
+                }
+            }
+        }
+        
+        var time = 0
+        var positions = Set<Point>([start])
+        while true {
+            time += 1
+            let winds = windPositions(at: time, starts: windStarts, width: width, height: height)
+            
+            let newPositions = positions.flatMap { move in
+                ([move] + move.adjacent).filter {
+                    !walls.contains($0) && !winds.contains($0)
+                }
+            }
+            positions = Set(newPositions)
+            if positions.contains(end) {
+                return time.description
+            }
+        }
+    }
+    
+    func windPositions(at: Int, starts: [Point: Set<Point>], width: Int, height: Int) -> Set<Point> {
+        var winds = Set<Point>()
+        
+        starts[.left]?.forEach { wind in
+            var newWind = wind + (Point.left * (at % width))
+            if newWind.x <= 0 {
+                newWind += Point.right * width
+            }
+            winds.insert(newWind)
+        }
+        starts[.right]?.forEach { wind in
+            var newWind = wind + (Point.right * (at % width))
+            if newWind.x >= width + 1 {
+                newWind += Point.left * width
+            }
+            winds.insert(newWind)
+        }
+        starts[.up]?.forEach { wind in
+            var newWind = wind + (Point.up * (at % height))
+            if newWind.y <= 0 {
+                newWind += Point.down * height
+            }
+            winds.insert(newWind)
+        }
+        starts[.down]?.forEach { wind in
+            var newWind = wind + (Point.down * (at % height))
+            if newWind.y >= height + 1 {
+                newWind += Point.up * height
+            }
+            winds.insert(newWind)
+        }
+        
+        return winds
     }
 }
--- Day24/Day24Tests.swift
+++ Day24/Day24Tests.swift
@@ -13,7 +13,41 @@ final class Day24Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+#.######
+#>>.<^<#
+#.<..<<#
+#>v.><>#
+#<^v^^>#
+######.#
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "18")
+    }
+    
+    func testWind() throws {
+        let windStarts: [Point: Set<Point>] = [
+            .up: [[1,1]],
+            .down: [[1,1]],
+            .left: [[1,1]],
+            .right: [[1,1]],
+        ]
+        
+        XCTAssertEqual(day.windPositions(at: 1, starts: windStarts, width: 10, height: 10), [[1, 10], [2, 1], [1, 2], [10, 1]])
+        XCTAssertEqual(day.windPositions(at: 5, starts: windStarts, width: 10, height: 10), [[1, 6], [6, 1], [1, 6], [6, 1]])
+        XCTAssertEqual(day.windPositions(at: 10, starts: windStarts, width: 10, height: 10), [[1, 1]])
+        XCTAssertEqual(day.windPositions(at: 11, starts: windStarts, width: 10, height: 10), [[1, 10], [2, 1], [1, 2], [10, 1]])
+        XCTAssertEqual(day.windPositions(at: 62, starts: windStarts, width: 10, height: 10), [[1, 9], [3, 1], [1, 3], [9, 1]])
+    }
+    
+    func testSampleWind() throws {
+        let windStarts: [Point: Set<Point>] = [
+            .up: [[5,1], [2,4], [4,4], [5,4]],
+            .down: [[1,1], [2,3], [3,4]],
+            .left: [[4,1], [6,1], [2,2], [5,2], [6,2], [5,3], [1,4]],
+            .right: [[1,1], [2,1], [1,3], [4,3], [6,3], [6,4]],
+        ]
+        
+        let wind1 = day.windPositions(at: 1, starts: windStarts, width: 6, height: 4)
+        wind1.printPoints()
+        XCTAssertEqual(wind1, [[2, 1], [3, 1], [5, 1], [1, 2], [5, 2], [4, 2], [1, 3], [2, 3], [4, 3], [5, 3], [1, 4], [2, 4], [5, 4], [6, 4]])
     }
 }
