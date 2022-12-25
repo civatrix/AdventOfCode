d91b813ae6d3ac14469f479d24b78f5f58ed80db
Daniel <civatrix@gmail.com>
2022-12-24 22:06:23 -0800

    Day 25 Part 1 and 2

--- Day25/Day25.swift
+++ Day25/Day25.swift
@@ -9,6 +9,69 @@ import Foundation
 
 final class Day25: Day {
     func run(input: String) -> String {
-        return ""
+        return input.lines.map {
+            $0.reversed().enumerated().map { (index, character) in
+                let base: Int
+                switch character {
+                case "1": base = 1
+                case "2": base = 2
+                case "-": base = -1
+                case "=": base = -2
+                case "0": base = 0
+                default: exit(2)
+                }
+                
+                return 5.power(to: index) * base
+            }.sum
+        }
+        .sum
+        .snafu
+    }
+}
+
+extension Int {
+    func power(to: Int) -> Int {
+        [Int](repeating: self, count: to).reduce(1, *)
+    }
+    
+    var snafu: String {
+        var result = [Int: [Int]]()
+        var mutable = self
+        var index = 0
+        while mutable >= 1 {
+            let remainder = mutable % 5
+            mutable /= 5
+            
+            switch remainder {
+            case 0: result[index, default: []].append(0)
+            case 1: result[index, default: []].append(1)
+            case 2: result[index, default: []].append(2)
+            case 3:
+                result[index, default: []].append(-2)
+                result[index + 1, default: []].append(1)
+            case 4:
+                result[index, default: []].append(-1)
+                result[index + 1, default: []].append(1)
+            default: exit(3)
+            }
+            
+            index += 1
+            while result.values.contains(where: { $0.sum == 3 }) {
+                let key = result.keys.sorted().first { result[$0]!.sum == 3 }!
+                result[key] = [-2]
+                result[key + 1, default: []].append(1)
+            }
+        }
+        
+        return result.sorted { $0.key < $1.key }.map { $0.value.sum }.reversed().map { number in
+            switch number {
+            case -2: return "="
+            case -1: return "-"
+            case 0: return "0"
+            case 1: return "1"
+            case 2: return "2"
+            default: exit(4)
+            }
+        }.joined()
     }
 }
--- Day25/Day25Tests.swift
+++ Day25/Day25Tests.swift
@@ -13,7 +13,24 @@ final class Day25Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+1=-0-2
+12111
+2=0=
+21
+2=01
+111
+20012
+112
+1=-1=
+1-12
+12
+1=
+122
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "2=-1=0")
+    }
+    
+    func test2022() throws {
+        XCTAssertEqual(2022.snafu, "1=11-2")
     }
 }
